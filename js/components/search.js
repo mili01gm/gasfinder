0e1111a862d1ce7c84743455f2068066bbac3ce9
Luz Gutierrez <mili01gm@gmail.com>
2017-06-06 14:54:40 -0500

    avance header

---
+++ js/components/search.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const Search = () => {
+    const parent = $('<div class="search"></div>');
+    const container = $('<div class="container"></div>');
+    const row = $('<div class="row"></div>');
+    const col = $('<div class="col-xs-12 col-sm-12 col-md-12"></div>');
+    const inputGroup = $('<div class="input-group"></div>');
+    const inputBtn = $('<span class="input-group-btn"></span');
+    const button = $('<button class="btn btn-default border-none" type="button"><span class="fa fa-search"></span></button>');
+    const input = $();
+}

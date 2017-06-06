6aa79e2bfaca681c67b09681931d66495c29dd08
Luz Gutierrez <mili01gm@gmail.com>
2017-06-06 15:15:52 -0500

    maquetado

--- js/components/search.js
+++ js/components/search.js
@@ -2,11 +2,21 @@
 
 const Search = () => {
     const parent = $('<div class="search"></div>');
-    const container = $('<div class="container"></div>');
+    const container = $('<div class="container-fluid"></div>');
     const row = $('<div class="row"></div>');
     const col = $('<div class="col-xs-12 col-sm-12 col-md-12"></div>');
     const inputGroup = $('<div class="input-group"></div>');
     const inputBtn = $('<span class="input-group-btn"></span');
     const button = $('<button class="btn btn-default border-none" type="button"><span class="fa fa-search"></span></button>');
-    const input = $();
+    const input = $('<input type="text" class="form-control" placeholder="Ingresa el distrito a buscar">');
+
+    inputBtn.append(button);
+    inputGroup.append(inputBtn);
+    inputGroup.append(input);
+    col.append(inputGroup);
+    row.append(col);
+    container.append(row);
+    parent.append(container);
+
+    return parent;
 }

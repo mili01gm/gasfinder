9f21e7baf12cc894a3ccf0c215dfa92e2c3f1912
Luz Gutierrez <mili01gm@gmail.com>
2017-06-07 12:39:08 -0500

    maqueta de detalle

--- js/components/searchDetails.js
+++ js/components/searchDetails.js
@@ -2,5 +2,15 @@
 
 const StationDetail = (update) => {
     const infoStation = $('<div class="info"></div>');
+    const name = $('<h5>' + state.selectedStation.name + '</h5>');
+    const adress = $('<p>' + state.selectedStation.address + '</p>');
+    const listProduct = $('<div class="listProduct"></div>');
+    const back = $('<a href="#"></a>');
+    const iconBack = $('<i class="fa fa-chevron-left"></i>');
+
+    state.selectedStation.products.forEach((e) => {
+        const product = $('<div class="product">' + elem + '</div>');
+        listProduct.append(product);
+    });
 
 }
